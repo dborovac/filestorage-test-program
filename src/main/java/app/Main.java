182bcacc2fa3b6236a9bbc4a0c33ebe9a33158ba
Dario Borovac <borovacdario4@gmail.com>
2021-11-15 22:05:18 +0100

    .

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,10 +1,13 @@
 package app;
 
+import dnl.utils.text.table.TextTable;
 import exceptions.FileStorageException;
 import fs.FileStorage;
 import fs.MyFile;
+import fs.StorageConfiguration;
 import input.UserInput;
-import user.User;
+import user.Credentials;
+import user.Privileges;
 import utils.StorageManager;
 
 import java.util.List;
@@ -20,66 +23,101 @@ public class Main {
             exception.printStackTrace();
         }
         FileStorage fs = StorageManager.getStorage();
-        System.out.printf("Inicijalizacija novog skladista '%s'\n", args[0]);
-        fs.init(args[0], UserInput.inputUsernameAndPassword());
-        System.out.printf("Trenutno skladiste: '%s'\n", args[0]);
-        int selection;
+        initialStorage(args[0]);
+        int selection = 0;
         do {
-            selection = UserInput.mainMenu();
             try {
+                selection = UserInput.mainMenu();
                 if (selection == 1) {
-                    String storageName = UserInput.inputStorageName();
-                    fs.init(storageName, UserInput.inputUsernameAndPassword());
-                    System.out.println("Trenutno skladiste: '" + storageName + "'");
+                    String storagePath = UserInput.inputPath();
+                    Credentials credentials = UserInput.inputCredentials();
+                    fs.init(storagePath, credentials);
+                    System.out.printf("Uspesno ste inicijalizovali novo skladiste '%s'! Trenutni korisnik: %s.\n", storagePath, credentials);
                 }
                 if (selection == 2) {
-                    String storageName = UserInput.inputStorageName();
-                    fs.login(storageName, UserInput.inputUsernameAndPassword());
-                    System.out.println("Trenutno skladiste: '" + storageName + "'");
+                    String storagePath = UserInput.inputPath();
+                    Credentials credentials = UserInput.inputCredentials();
+                    fs.login(storagePath, credentials);
+                    System.out.printf("Uspesno ste se ulogovali u skladiste '%s'! Trenutni korisnik: %s.\n", storagePath, credentials.toString());
                 }
                 if (selection == 3) {
                     fs.logout();
                     System.out.println("Uspesno ste se izlogovali.");
                 }
                 if (selection == 4) {
-                    fs.addUser(UserInput.inputUsernameAndPassword(), UserInput.inputPrivileges());
-                    System.out.println("Uspesno ste dodali novog korisnika.");
+                    Credentials credentials = UserInput.inputCredentials();
+                    Privileges privileges = UserInput.inputPrivileges();
+                    fs.addUser(credentials, privileges);
+                    System.out.printf("Uspesno ste dodali novog %s korisnika.\n", privileges.getType().toString());
                 }
                 if (selection == 5) {
                     int ls;
+                    String path;
                     do {
                         ls = UserInput.lsMenu();
-                        if (ls == 1) print_ls(fs.ls());
-                        if (ls == 2) print_ls(fs.lsFiles());
-                        if (ls == 3) print_ls(fs.lsDirectories());
-                        if (ls == 4) print_ls(fs.lsByType(UserInput.inputFileType()));
-                        if (ls == 5) print_ls(fs.lsSortedByName(UserInput.inputSortOrder()));
-                        if (ls == 6) print_ls(fs.lsSortedByDate(UserInput.inputSortOrder()));
-                        if (ls == 7) print_ls(fs.lsSortedByLastModified(UserInput.inputSortOrder()));
-                    } while (ls != 0);
+                        if (ls == 0) break;
+                        path = UserInput.inputPath();
+                        if (ls == 1) print_ls(fs.lsAll(path));
+                        if (ls == 2) print_ls(fs.lsFiles(path));
+                        if (ls == 3) print_ls(fs.lsDirectories(path));
+                        if (ls == 4) print_ls(fs.lsByType(path, UserInput.inputFileType()));
+                        if (ls == 5) print_ls(fs.lsSortedByName(path, UserInput.inputSortOrder()));
+                        if (ls == 6) print_ls(fs.lsSortedByDate(path, UserInput.inputSortOrder()));
+                        if (ls == 7) print_ls(fs.lsSortedByLastModified(path, UserInput.inputSortOrder()));
+                    } while (true);
                 }
                 if (selection == 6) {
                     fs.makeFiles(UserInput.inputPattern(), UserInput.inputPath());
                 }
                 if (selection == 7) {
-                    fs.deleteFile(UserInput.inputPath());
+                    String path = UserInput.inputPath();
+                    fs.deleteFile(path);
+                    System.out.printf("Uspesno ste obrisali fajl %s.\n", path);
                 }
                 if (selection == 8) {
-                    fs.downloadFile(UserInput.inputPath());
+                    String path = UserInput.inputPath();
+                    fs.downloadFile(path);
+                    System.out.printf("Uspesno ste preuzeli fajl %s u Downloads folder!\n", path);
                 }
-            } catch (FileStorageException exception) {
+                if (selection == 9) {
+                    String source = UserInput.inputSourcePath();
+                    String destination = UserInput.inputDestinationPath();
+                    fs.moveFile(source, destination);
+                    System.out.printf("Uspesno ste premestili %s u %s\n", source, destination);
+                }
+                if (selection == 10) {
+                    StorageConfiguration storageConfiguration = UserInput.inputConfiguration();
+                    fs.configureStorage(storageConfiguration);
+                    System.out.println("Uspesno ste konfigurisali skladiste! " + storageConfiguration.toString());
+                }
+            } catch (FileStorageException | NumberFormatException exception) {
                 System.out.println(exception.getMessage());
             }
         } while (selection != 0);
     }
 
+    private static void initialStorage(String name) {
+        FileStorage fs = StorageManager.getStorage();
+        System.out.printf("Inicijalizacija novog skladista '%s'\n", name);
+        Credentials credentials = UserInput.inputCredentials();
+        fs.init(name, credentials);
+        System.out.printf("Trenutno skladiste: '%s'. Trenutni korisnik: %s.\n", name, credentials.getUsername());
+    }
+
     private static void print_ls(List<MyFile> files) {
-        System.out.println("---------------------------------------------------------------------------");
-        System.out.println("Name \t\t\t Created time \t\t\t Modified time \t\t\t Extension");
-        System.out.println("---------------------------------------------------------------------------");
-        for (MyFile file : files) {
-            System.out.println(file.getFileName() + "\t" + file.getDate() + "\t" + file.getLastModified() + "\t" + file.getType());
+        TextTable tt = new TextTable(new String[]{"Name", "Created time", "Modified time", "Extension"}, toPrimitiveArray(files));
+        tt.printTable();
+        System.out.println("----------------------------------------------------------------------------");
+    }
+
+    private static Object[][] toPrimitiveArray(List<MyFile> files) {
+        Object[][] out = new Object[files.size()][4];
+        for (int i = 0; i < files.size(); i++) {
+            out[i][0] = files.get(i).getFileName();
+            out[i][1] = files.get(i).getDate();
+            out[i][2] = files.get(i).getLastModified();
+            out[i][3] = files.get(i).getType();
         }
-        System.out.println("---------------------------------------------------------------------------");
+        return out;
     }
 } // -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005
--- src/main/java/input/UserInput.java
+++ src/main/java/input/UserInput.java
@@ -1,6 +1,9 @@
 package input;
 
+import fs.StorageConfiguration;
+import user.Credentials;
 import user.PrivilegeTypes;
+import user.Privileges;
 import utils.SortOrder;
 
 import java.util.*;
@@ -16,7 +19,9 @@ public class UserInput {
         System.out.println("5. Prikaz fajlova.");
         System.out.println("6. Kreiranje direktorijuma i fajlova.");
         System.out.println("7. Brisanje direktorijuma i fajlova.");
-        System.out.println("8. Preuzimanje fajla.");
+        System.out.println("8. Preuzimanje fajlova.");
+        System.out.println("9. Premestanje fajlova.");
+        System.out.println("10. Konfiguracija skladista.");
         System.out.println("0. Izlaz.");
         return Integer.parseInt(input.nextLine());
     }
@@ -33,38 +38,49 @@ public class UserInput {
         return Integer.parseInt(input.nextLine());
     }
 
-    public static String inputUsernameAndPassword() {
+    public static Credentials inputCredentials() {
         System.out.println("Unesite korisnicko ime: ");
         String username = input.nextLine();
         System.out.println("Unesite lozinku: ");
         String password = input.nextLine();
-        return username + ":" + password;
+        return new Credentials(username, password);
     }
 
-    public static String inputStorageName() {
-        System.out.println("Unesite ime skladista: ");
-        return input.nextLine();
-    }
-
-    public static PrivilegeTypes inputPrivileges() {
+    public static Privileges inputPrivileges() {
         int selection;
         do {
             selection = privilegeMenu();
             if (selection == 1) {
-                return PrivilegeTypes.ReadWrite;
+                Privileges privileges = new Privileges(PrivilegeTypes.ReadWrite);
+                boolean canDownload = inputCanDownload();
+                privileges.setDownload(canDownload);
+                return privileges;
             }
             if (selection == 2) {
-                return PrivilegeTypes.ReadOnly;
+                Privileges privileges = new Privileges(PrivilegeTypes.ReadOnly);
+                boolean canDownload = inputCanDownload();
+                privileges.setDownload(canDownload);
+                return privileges;
             }
             if (selection == 3) {
-                return PrivilegeTypes.None;
+                return new Privileges(PrivilegeTypes.None);
             }
         } while (true);
     }
 
     private static int privilegeMenu() {
-        System.out.println("Unesite nivo privilegije; 1-ReadWrite, 2-ReadOnly, 3-None");
+        System.out.println("Unesite nivo privilegije; 1-ReadWrite, 2-ReadOnly, 3-None:");
         return Integer.parseInt(input.nextLine());
+    }
+
+    private static boolean inputCanDownload() {
+        String canDownload;
+        do {
+            System.out.println("Da li zelite da omogucite preuzimanje fajlova ovom korisniku? (da/ne)");
+            canDownload = input.nextLine();
+            if (canDownload.equals("da")) return true;
+            if (canDownload.equals("ne")) return false;
+        } while (true);
     }
 
     public static String inputFileType() {
@@ -73,7 +89,7 @@ public class UserInput {
     }
 
     public static SortOrder inputSortOrder() {
-        System.out.println("Unesite 'r' za rastuce ili 'o' za opadajuce");
+        System.out.println("Unesite 'r' za rastuce ili 'o' za opadajuce:");
         String sortOrder;
         do {
             sortOrder = input.nextLine();
@@ -87,8 +103,18 @@ public class UserInput {
         return input.nextLine();
     }
 
+    public static String inputSourcePath() {
+        System.out.println("Unesite izvornu putanju: ");
+        return input.nextLine();
+    }
+
+    public static String inputDestinationPath() {
+        System.out.println("Unesite odredisnu putanju: ");
+        return input.nextLine();
+    }
+
     public static String inputPattern() {
-        System.out.println("Zadajte sablon za kreiranje direktorijuma i fajlova. Unesite 'h' za pomoc ili 'n' da idete nazad.");
+        System.out.println("Zadajte sablon za kreiranje direktorijuma i fajlova. Unesite 'h' za pomoc.");
         String in;
         do {
             in = input.nextLine();
@@ -100,9 +126,50 @@ public class UserInput {
                 System.out.println("mkdir/mkfile fajl{1..20}     - kreira 20 fajlova pod imenom fajl1,fajl2...fajl20");
                 System.out.println("mkdir/mkfile random{n}     - kreira n fajlova sa imenom sastavljenim od 10 nasumicno izabranih karaktera.");
                 System.out.println("---------------------------------------------------------------------------");
-            } else if (in.equals("n")) break;
-            else return in;
+            } else return in;
         } while (true);
-        return "";
+    }
+
+    public static StorageConfiguration inputConfiguration() {
+        StorageConfiguration storageConfiguration = new StorageConfiguration();
+        String selection;
+        do {
+            System.out.println("1. Konfiguracija maksimalne velicine skladista.");
+            System.out.println("2. Konfiguracija nedozvoljenih fajl ekstenzija.");
+            System.out.println("3. Konfiguracija maksimalnog broja fajlova po folderu.");
+            System.out.println("0. Nazad");
+            selection = input.nextLine();
+            if (selection.equals("1")) inputMaxStorageSize(storageConfiguration);
+            if (selection.equals("2")) inputRestrictedTypes(storageConfiguration);
+            if (selection.equals("3")) inputFileCountRestrictions(storageConfiguration);
+        } while (!selection.equalsIgnoreCase("0"));
+        return storageConfiguration;
+    }
+
+    private static void inputMaxStorageSize(StorageConfiguration storageConfiguration) {
+        System.out.println("Unesite maksimalnu velicinu skladista (u bajtovima):");
+        storageConfiguration.setMaxSize(Long.parseLong(input.nextLine()));
+    }
+
+    private static void inputRestrictedTypes(StorageConfiguration storageConfiguration) {
+        String ext;
+        do {
+            System.out.println("Unesite ekstenziju. Unesite 0 da idete nazad:");
+            ext = input.nextLine();
+            if (!ext.equals("0")) storageConfiguration.addRestrictedType(ext);
+        } while (!ext.equals("0"));
+    }
+
+    private static void inputFileCountRestrictions(StorageConfiguration storageConfiguration) {
+        String folder;
+        int count;
+        do {
+            System.out.println("Unesite ime foldera. Unesite 0 da idete nazad:");
+            folder = input.nextLine();
+            if (folder.equals("0")) break;
+            System.out.println("Unesite broj fajlova:");
+            count = Integer.parseInt(input.nextLine());
+            storageConfiguration.addFileCountRestriction(folder, count);
+        } while (true);
     }
 }
